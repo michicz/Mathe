7aacce7feea45f774c51de2cd1d287cffee05cce
michael <michicz@gmail.com>
2025-12-04 12:12:37 +0100

    2nd draft

---
+++ test.py
@@ -0,0 +1,7 @@
+
+
+print('Kopfrechentrainer 0.1')
+selection = input('Auswahl:\nEinmaleins: e, Runden: r, Quersumme: q')
+if selection in ('e', 'r', 'q'):
+    if selection == 'e':
+        print('einmaleins ausgewählt')
